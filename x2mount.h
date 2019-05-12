eedbe4c6d55d632a7c231f78a493dcad47d7eb58
Rodolphe Pineau <pineau@rti-zone.org>
2019-05-12 11:50:51 -0700

    uprev to 1.06

--- x2mount.h
+++ x2mount.h
@@ -28,7 +28,7 @@
 // Include files for iOptron mount
 #include "iOptronV3.h"
 
-#define DRIVER_VERSION 1.05
+#define DRIVER_VERSION 1.06
 
 #define PARENT_KEY			"iOptronV3"
 #define CHILD_KEY_PORT_NAME "PortName"
