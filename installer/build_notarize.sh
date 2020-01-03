0f0548acc4c2dc88aca775558f428f39e9da2e8b
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-03 11:34:56 -0800

    Fix notarize script

--- installer/build_notarize.sh
+++ installer/build_notarize.sh
@@ -15,20 +15,21 @@ cp "../build/Release/libiOptronV3.dylib" ROOT/tmp/iOptronV3_X2/
 
 if [ ! -z "$installer_signature" ]; then
 	# signed package using env variable installer_signature
-	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
-	pkgutil --check-signature ./${PACKAGE_NAME}
-	res=`xcrun altool --notarize-app --primary-bundle-id $BUNDLE_NAME --username "$AC_USERNAME" --password "@keychain:AC_PASSWORD" --file $PACKAGE_NAME`
+	pkgbuild --root ROOT --identifier "$BUNDLE_NAME" --sign "$installer_signature" --scripts Scripts --version 1.0 "$PACKAGE_NAME"
+	pkgutil --check-signature "./${PACKAGE_NAME}"
+	res=`xcrun altool --notarize-app --primary-bundle-id "$BUNDLE_NAME" --username "$AC_USERNAME" --password "@keychain:AC_PASSWORD" --file $PACKAGE_NAME`
 	RequestUUID=`echo $res | grep RequestUUID | cut -d"=" -f 2 | tr -d [:blank:]`
 	if [ -z "$RequestUUID" ]; then
 		echo "Error notarizing"
+		echo "res = $res"
 		exit 1
 	fi
-	echo "Notarization RequestUUID $RequestUUID"
+	echo "Notarization RequestUUID '$RequestUUID'"
 	sleep 30
 	while true
 	echo "Waiting for notarization"
 	do
-		res=`xcrun altool --notarization-info $RequestUUID --username "pineau@rti-zone.org" --password "@keychain:AC_PASSWORD"`
+		res=`xcrun altool --notarization-info "$RequestUUID" --username "pineau@rti-zone.org" --password "@keychain:AC_PASSWORD"`
 		pkg_ok=`echo $res | grep -i "Package Approved"`
 		pkg_in_progress=`echo $res | grep -i "in progress"`
 		if [ ! -z "$pkg_ok" ]; then
@@ -37,6 +38,7 @@ if [ ! -z "$installer_signature" ]; then
 			sleep 60
 		else
 			echo  "Notarization timeout or error"
+			echo "res = $res"
 			exit 1
 		fi
 	done
